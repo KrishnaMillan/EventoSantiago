f5498e9607e0529e59a27c7610577103ef057073
KrishnaMillan <45037085+KrishnaMillan@users.noreply.github.com>
2019-05-01 12:56:24 -0400

    Add files via upload

--- App/forms.py
+++ App/forms.py
@@ -1,27 +1,6 @@
 from django import forms
 
-class AgregaUsuario(forms.Form):
-	nombre=forms.CharField(widget=forms.TextInput(), label="Nombre")
-	rut=forms.CharField(widget=forms.TextInput(), label="Rut")
-	fec_nac=forms.DateField(widget=forms.DateInput(),label="Fecha de Nacimiento")
-	correoAsociado=forms.CharField(widget=forms.TextInput(), label="Correo")
-	comuna=forms.CharField(widget=forms.TextInput(), label="Comuna")
-	contrasena=forms.CharField(widget=forms.PasswordInput(), label="Contraseña")
-	contrasena2=forms.CharField(widget=forms.PasswordInput(), label="Repetir Contraseña")
 
 class Login(forms.Form):
-	rut=forms.CharField(widget=forms.TextInput(), label="Rut")
-	contrasena=forms.CharField(widget=forms.PasswordInput(), label="Contraseña")
-
-class ActualizaUsuario(forms.Form):
-	nombre=forms.CharField(widget=forms.TextInput(), label="Nombre", required=False)
-	fec_nac=forms.DateField(widget=forms.DateInput(),label="Fecha de Nacimiento", required=False)
-	comuna=forms.CharField(widget=forms.TextInput(), label="Comuna", required=False)
-	correoAsociado=forms.CharField(widget=forms.EmailInput(), label="Correo", required=False)
-
-class RecuperaContrasena(forms.Form):
-	rut=forms.CharField(widget=forms.TextInput(), label="Rut")
-
-class RecuperaContrasena2(forms.Form):	
-	contrasena=forms.CharField(widget=forms.PasswordInput(), label="Contraseña nueva",required=False)
-	contrasena2=forms.CharField(widget=forms.PasswordInput(), label="Repetir Contraseña nueva",required=False)
+	rut=forms.CharField(widget=forms.TextInput(), label="Rut", required=True)
+	contrasena=forms.CharField(widget=forms.PasswordInput(), label="Contraseña", required=True)
