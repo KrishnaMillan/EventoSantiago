cf482f38c6bb2f3e8c3713494cba810ded7bdb23
AnaYaniraSS <49693430+AnaYaniraSS@users.noreply.github.com>
2019-05-03 22:31:07 -0400

    Add files via upload

--- App/forms.py
+++ App/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class Login(forms.Form):
-	rut=forms.CharField(widget=forms.TextInput(), label="Rut", required=True)
-	contrasena=forms.CharField(widget=forms.PasswordInput(), label="Contraseña", required=True)
+
+	rut=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Sin puntos ni guión'}), label="Rut", required=True)
+	contrasena=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': '*******'}), label="Contraseña", required=True)
