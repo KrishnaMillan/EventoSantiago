2ddcc9b9e16ed10f7f9a751d17e94cc899416edd
KrishnaMillan <45037085+KrishnaMillan@users.noreply.github.com>
2019-06-12 14:33:53 -0400

    Add files via upload

--- App/views.py
+++ App/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, render_to_response, redirect
-from django.template import loader
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import Login
 from .models import Cuenta, Usuario, RegistroEmail, Empresa, Administrador, Evento, Reserva, Visita
@@ -30,7 +29,7 @@ def misReservas(request):
 			mensaje = "Error al cancelar"
 			messages.error(request, mensaje)
 
-	return render(request, 'misReservas.html',{'reservas':reservas})
+	return render(request, 'misreservas.html',{'reservas':reservas})
 
 def misEventos(request):
 	event = Evento.objects.all()
@@ -52,11 +51,11 @@ def misEventos(request):
 			else:
 				mensaje = "Error al reservar, no hay cupos!!!!"
 				messages.error(request, mensaje)
-			
+
 		except:
 			mensaje = "Error al reservar"
 			messages.error(request, mensaje)
-	return render(request, 'misEventos.html', {'event':event, 'misevents':misevents})
+	return render(request, 'miseventos.html', {'event':event, 'misevents':misevents})
 
 def eliminarEvento(request, id_evento):
 	event = Evento.objects.get(id_evento = id_evento)
@@ -72,6 +71,7 @@ def eliminarEvento(request, id_evento):
 
 def crearEvento(request):
 	variables = {}
+	variables['mensaje']=''
 	if request.POST:
 		event = Evento()
 		event.fecha = request.POST.get('fecha')
@@ -85,7 +85,7 @@ def crearEvento(request):
 		event.Empresa = Empresa.objects.get(Cuenta=Cuenta.objects.get(rut=request.user.get_username()))
 		try:
 			event.save()
-			variables['mensaje'] = 'Evento Guardado' 
+			variables['mensaje'] = 'Evento Guardado'
 		except:
 			variables['mensaje'] = 'Error al guardar'
 	return render(request, 'crearEvento.html', variables)
@@ -103,7 +103,7 @@ def modificarEvento(request, id_evento):
 		event.id_evento = request.POST.get('id')
 		event.fecha = request.POST.get('fecha')
 		event.hora = request.POST.get('hora')
-		event.comuna = request.POST.get('comuna')		
+		event.comuna = request.POST.get('comuna')
 		event.nombre=request.POST.get('nombre')
 		event.descripcion=request.POST.get('descripcion')
 		event.direccion = request.POST.get('direccion')
@@ -135,7 +135,7 @@ def registrarse(request):
 			mensaje="Este rut ya está registrado, ingrese los datos nuevamente"
 		except:
 			if contrasena==contrasena2:
-				
+
 				user=User.objects.create_user(username=rut,password=contrasena,is_active=True)
 				user.save()
 				cuenta=Cuenta(rut=rut, nombre=nombre, correoAsociado=correoAsociado, user=user, comuna=comuna, fecha_registro=fecha_registro)
@@ -148,9 +148,9 @@ def registrarse(request):
 				except Group.DoesNotExist:
 					Group.objects.create(name="Usuarios")
 					user.groups.add(Group.objects.get(name="Usuarios"))
-			else: 
+			else:
 				mensaje="Las contraseñas no coinciden"
-			
+
 
 	return render(request, "registrarse.html",{'mensaje':mensaje})
 
@@ -165,7 +165,7 @@ def ingresar(request):
 			correo=cuenta.correoAsociado
 			user=authenticate(username=data.get("rut"),password=data.get("contrasena"))
 			users=User.objects.get(username=data.get("rut"))
-			
+
 			if user is not None:
 					login(request, user)
 					return redirect('index')
@@ -177,10 +177,10 @@ def ingresar(request):
 				registro=RegistroEmail(Cuenta=cuenta, iduuid=iduuid)
 				registro.save()
 				mensaje="Tu cuenta está inactiva, se envió un link a tu correo electrónico para reactivarla"
-				send_mail('Reactiva tu cuenta','','eventosantiago7@gmail.com',[correo],html_message='Este email fue enviado porque tu cuenta está inactiva e intentaste iniciar sesión <br><a href="http://krishnamillan.pythonanywhere.com/reactivar?iduuid='+iduuid+'&user='+users.username+'">Click aquí para reactivar tu cuenta</a><br>si no fuiste tu ignora este email')	
+				send_mail('Reactiva tu cuenta','','eventosantiago7@gmail.com',[correo],html_message='Este email fue enviado porque tu cuenta está inactiva e intentaste iniciar sesión <br><a href="http://krishnamillan.pythonanywhere.com/reactivar?iduuid='+iduuid+'&user='+users.username+'">Click aquí para reactivar tu cuenta</a><br>si no fuiste tu ignora este email')
 			if users.is_active==False and User.objects.filter(username=data.get("rut"), groups__name='Empresas').exists():
 				mensaje="Tu cuenta de empresa, aún no ha sido activada"
-				
+
 	except:
 		mensaje="No se ha encontrado el usuario"
 	return render(request,"login.html",{'form':form,'mensaje':mensaje})
@@ -203,15 +203,17 @@ def eliminar(request):
 	user=User.objects.get(username=request.user.get_username())
 	user.is_active=False
 	user.save()
-	usuario=Reserva.objects.filter(Usuario=Usuario.objects.get(Cuenta=Cuenta.objects.get(rut=request.user.get_username())))
-	usuario.delete()
 	if User.objects.filter(username=request.user.get_username(), groups__name='Usuarios').exists():
-		mensaje="Desactivada"
+	    usuario=Reserva.objects.filter(Usuario=Usuario.objects.get(Cuenta=Cuenta.objects.get(rut=request.user.get_username())))
+	    usuario.delete()
+	    mensaje="Desactivada"
+
 	if User.objects.filter(username=request.user.get_username(), groups__name='Empresas').exists():
-		visita=Visita.objects.get(Empresa=(Empresa.objects.get(Cuenta=Cuenta.objects.get(rut=request.user.get_username()))))		
+		visita=Visita.objects.get(Empresa=(Empresa.objects.get(Cuenta=Cuenta.objects.get(rut=request.user.get_username()))))
 		visita.resolucion="La cuenta fue desactivada el: "+ time.strftime("%Y-%m-%d")
 		visita.save()
 		mensaje="Empresa Desactivada"
+
 	logout(request)
 	return render(request,'index.html', {'mensaje':mensaje})
 @login_required(login_url='ingresar')
@@ -224,19 +226,19 @@ def actualizar(request):
 		correoAsociado=request.POST.get("correoAsociado")
 		comuna=request.POST.get("comuna")
 		mensaje="Todos los campos estan en blanco, no se han realizado cambios"
-		
+
 		if(nombre!=None and nombre!=""):
 			Cuenta.objects.filter(rut=request.user.get_username()).update(nombre=nombre)
 			mensaje="Datos actualizados correctamente"
-			
+
 		if(correoAsociado!=None and correoAsociado!=""):
 			Cuenta.objects.filter(rut=request.user.get_username()).update(correoAsociado=correoAsociado)
 			mensaje="Datos actualizados correctamente"
-			
+
 		if(comuna!=None and comuna!=""):
 			Cuenta.objects.filter(rut=request.user.get_username()).update(comuna=comuna)
 			mensaje="Datos actualizados correctamente"
-			
+
 	return render(request,"actualizar.html",{'usuario':usuario,'mensaje':mensaje})
 
 def recuperar(request):
@@ -291,7 +293,7 @@ def cambiarcontrasena(request):
 		contrasena=request.POST.get("contrasena1")
 		contrasena2=request.POST.get("contrasena2")
 		usuario=User.objects.get(username=request.user.get_username())
-		
+
 		if(usuario.check_password(request.POST.get('actual'))):
 			if contrasena==contrasena2:
 				usuario.set_password(contrasena)
@@ -339,7 +341,7 @@ def registroEmpresa(request):
 			mensaje="Este rut ya está registrado, ingrese los datos nuevamente"
 		except:
 			if contrasena==contrasena2:
-				
+
 				user=User.objects.create_user(username=rut,password=contrasena,is_active=False)
 				user.save()
 				cuenta=Cuenta(rut=rut, nombre=nombre, correoAsociado=correoAsociado, user=user, comuna=comuna, fecha_registro=fecha_registro)
@@ -352,9 +354,9 @@ def registroEmpresa(request):
 				except Group.DoesNotExist:
 					Group.objects.create(name="Empresas")
 					user.groups.add(Group.objects.get(name="Empresas"))
-			else: 
+			else:
 				mensaje="Las contraseñas no coinciden"
-			
+
 
 	return render(request, "registroempresa.html",{'mensaje':mensaje})
 
@@ -376,7 +378,7 @@ def creaAdmin(request):
 			mensaje="Este rut ya está registrado, ingrese los datos nuevamente"
 		except:
 			if contrasena==contrasena2:
-				
+
 				user=User.objects.create_user(username=rut,password=contrasena,is_active=True)
 				user.save()
 				cuenta=Cuenta(rut=rut, nombre=nombre, correoAsociado=correoAsociado, user=user, comuna=comuna, fecha_registro=fecha_registro)
@@ -389,7 +391,7 @@ def creaAdmin(request):
 				except Group.DoesNotExist:
 					Group.objects.create(name="Administradores")
 					user.groups.add(Group.objects.get(name="Administradores"))
-			else: 
+			else:
 				mensaje="Las contraseñas no coinciden"
 	return render(request,"creaadmin.html",{'mensaje':mensaje})
 
@@ -419,14 +421,14 @@ def solicitudEmpresas(request):
 
 @login_required(login_url='ingresar')
 def detalleEmpresa(request):
-	
+
 	cuentas=Cuenta.objects.filter(user__in=User.objects.filter(groups__name='Empresas', is_active=False))
 	cuenta=Cuenta.objects.get(rut=request.GET.get("rut"))
 	resolucion=""
 	mensaje=""
 	visitas="No tiene visitas agendadas"
 	existe=False
-	
+
 	if(Visita.objects.filter(Empresa=(Empresa.objects.get(Cuenta=cuenta))).exists()):
 		visita=Visita.objects.get(Empresa=(Empresa.objects.get(Cuenta=cuenta)))
 		visitas=visita.fecha_visita
@@ -443,7 +445,7 @@ def detalleEmpresa(request):
 			return render(request,"solicitudempresas.html",{'mensaje':mensaje,'cuentas':cuentas})
 	if request.POST.get("guardarResolucion") is not None and existe:
 		resolucion=request.POST.get("resolucion")
-		Visita.objects.filter(Empresa=Empresa.objects.get(Cuenta=cuenta), Administrador=(Administrador.objects.get(Cuenta=(Cuenta.objects.get(rut=request.user.get_username()))))).update(resolucion=resolucion)	
+		Visita.objects.filter(Empresa=Empresa.objects.get(Cuenta=cuenta), Administrador=(Administrador.objects.get(Cuenta=(Cuenta.objects.get(rut=request.user.get_username()))))).update(resolucion=resolucion)
 		mensaje="Resolucion guardada correctamente"
 		return render(request,'solicitudempresas.html',{'mensaje':mensaje,'cuentas':cuentas})
 	if request.POST.get("activar") is not None and existe and resolucion!="":
